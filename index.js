0ad05f23c5e4fc1c4eec99c75aa69de235bcb86c
Trey Shugart <treshugart@gmail.com>
2015-11-09 16:29:29 +1100

    Simplify API and add a method for getting global options.

--- index.js
+++ index.js
@@ -2,17 +2,40 @@
 
 var assign = require('object-assign');
 var argv = require('minimist')(process.argv.slice(2));
+var fs = require('fs');
 var path = require('path');
-var loaded = [];
+
+var loaded = {};
+
+function resolvePath (p) {
+  return path.isAbsolute(p) ? p : path.join(process.cwd(), p);
+}
+
+function loadOptions () {
+  var opts;
+  var rcFile = path.join(process.cwd(), '.gulprc');
+
+  if (fs.existsSync(rcFile)) {
+    try {
+      opts = JSON.parse(fs.readFileSync(rcFile));
+    } catch (e) {
+      throw new Error('cannot parse config "' + rcFile + '" because: ' + e);
+    }
+  }
+
+  return assign({
+    base: '.',
+    gulp: 'node_modules/gulp'
+  }, opts, argv);
+}
 
 function loadTask (task, opts) {
   var func;
   var funcLoadError;
 
-  // Don't trace tasks if they've already been loaded.
-  if (loaded.indexOf(task) > -1) {
-    return;
-  }
+  // Don't recurse.
+  if (loaded[task]) { return; }
+  loaded[task] = task;
 
   // Find the first matching module.
   opts.base.some(function (base) {
@@ -24,53 +47,39 @@ function loadTask (task, opts) {
     }
   });
 
+  // Try and give helpful load errors.
   if (!func && funcLoadError) {
-    throw new Error ('could not load task "' + task + '" because it could not be found in ' + JSON.stringify(opts.base) + ' because: ' + funcLoadError);
+    throw new Error ('could not load task "' + task + '" from ' + JSON.stringify(opts.base) + ' because: ' + funcLoadError);
   }
 
-  // Flag so we can check for circular deps.
-  loaded.push(task);
-
-  // Tasks can be: module.exports = ['dependencies']
-  if (Array.isArray(func)) {
-    var deps = func;
-    func = function (opts, done) {
-      return done();
-    };
-    func.dependencies = deps;
-  }
-
-  // Don't register private tasks.
-  if (func.private) {
-    return;
-  }
-
-  (func.dependencies || []).forEach(function (dep) {
-    try {
-      loadTask(dep, opts);
-    } catch (e) {
-      throw new Error('could not load task dependency "' + dep + '" for "' + task + '" because: ' + e);
-    }
-  });
-
-  opts.gulp.task(task, func.bind(argv));
+  // Simple register.
+  opts.gulp.task(task, func);
 }
 
-module.exports = function (opts) {
-  opts = assign({
-    base: process.cwd(),
-    gulp: require('gulp')
-  }, opts);
+function loadTasks () {
+  var opts = loadOptions();
 
+  // Can specify the path to the Gulp module.
+  opts.gulp = require(resolvePath(opts.gulp));
+
+  // Base path can be a string or array of base paths.
   if (typeof opts.base === 'string') {
     opts.base = [opts.base];
   }
 
-  opts.base = opts.base.map(function (base) {
-    return path.isAbsolute(base) ? base : path.join(process.cwd(), base);
-  });
+  // Ensure all paths are normalised.
+  opts.base = opts.base.map(resolvePath);
 
+  // Load all specified tasks.
   argv._.forEach(function (task) {
     loadTask(task, opts);
   });
+
+  // Export the options so they can be used.
+  return opts;
+}
+
+module.exports = {
+  load: loadTasks,
+  opts: loadOptions
 };
